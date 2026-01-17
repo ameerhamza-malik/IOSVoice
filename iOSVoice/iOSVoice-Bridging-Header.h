98ba9e7a2239b336633c56b7608c28f50b6e9203
ameerhamza-malik <i221570@nu.edu.pk>
2026-01-17 20:43:10 +0500

    feat: Add iOSVoice bridging header for ONNX Runtime integration

---
+++ iOSVoice/iOSVoice-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  iOSVoice-Bridging-Header.h
+//  iOSVoice
+//
+
+#ifndef iOSVoice_Bridging_Header_h
+#define iOSVoice_Bridging_Header_h
+
+#import <onnxruntime.h>
+
+#endif /* iOSVoice_Bridging_Header_h */
